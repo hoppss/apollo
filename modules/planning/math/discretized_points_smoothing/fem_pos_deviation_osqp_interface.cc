5604312383ea4c7d14a1e1514e5fd46d79ca55e4
lm <lm@xx.com>
2023-08-21 20:43:42 +0800

    fem osqp

--- modules/planning/math/discretized_points_smoothing/fem_pos_deviation_osqp_interface.cc
+++ modules/planning/math/discretized_points_smoothing/fem_pos_deviation_osqp_interface.cc
@@ -33,12 +33,12 @@ bool FemPosDeviationOsqpInterface::Solve() {
     AERROR << "reference points empty, solver early terminates";
     return false;
   }
-
+  // 每个点都有bound约束
   if (ref_points_.size() != bounds_around_refs_.size()) {
     AERROR << "ref_points and bounds size not equal, solver early terminates";
     return false;
   }
-
+  // ref points 不能< 3， n-2 > 1
   if (ref_points_.size() < 3) {
     AERROR << "ref_points size smaller than 3, solver early terminates";
     return false;
@@ -51,16 +51,19 @@ bool FemPosDeviationOsqpInterface::Solve() {
 
   // Calculate optimization states definitions
   num_of_points_ = static_cast<int>(ref_points_.size());
-  num_of_variables_ = num_of_points_ * 2;
-  num_of_constraints_ = num_of_variables_;
+  num_of_variables_ = num_of_points_ * 2;  // 每个点都有x,y bound 约束
+  num_of_constraints_ = num_of_variables_; // osqp 只有bound约束，点数*2, 不考虑曲率约束
 
-  // Calculate kernel
+  // Calculate kernel  CSC matrix
+  // 目标函数  f = X^t * P * X + Q^t * x， P 为hessian matrix， Q 是offset max
+  // P 为正定矩阵， P 为对称矩阵， osqp 0.6.0 以上部分只需要填充上三角数据
   std::vector<c_float> P_data;
   std::vector<c_int> P_indices;
   std::vector<c_int> P_indptr;
   CalculateKernel(&P_data, &P_indices, &P_indptr);
 
   // Calculate affine constraints
+  // 等式不等式约束 lower_bound <= AX <= upper_bounds
   std::vector<c_float> A_data;
   std::vector<c_int> A_indices;
   std::vector<c_int> A_indptr;
@@ -73,7 +76,7 @@ bool FemPosDeviationOsqpInterface::Solve() {
   std::vector<c_float> q;
   CalculateOffset(&q);
 
-  // Set primal warm start
+  // Set primal warm start, 给初解
   std::vector<c_float> primal_warm_start;
   SetPrimalWarmStart(&primal_warm_start);
 
@@ -133,9 +136,9 @@ void FemPosDeviationOsqpInterface::CalculateKernel(
 
   // Three quadratic penalties are involved:
   // 1. Penalty x on distance between middle point and point by finite element
-  // estimate;
-  // 2. Penalty y on path length;
-  // 3. Penalty z on difference between points and reference points
+  // estimate; - FEM 平滑项
+  // 2. Penalty y on path length; - 长度等长， 均匀性
+  // 3. Penalty z on difference between points and reference points  - 几何相似性
 
   // General formulation of P matrix is as below(with 6 points as an example):
   // I is a two by two identity matrix, X, Y, Z represents x * I, y * I, z * I
@@ -147,18 +150,29 @@ void FemPosDeviationOsqpInterface::CalculateKernel(
   // |0,     0,       0,       0,       5X+2Y+Z, -2X-Y|
   // |0,     0,       0,       0,       0,       X+Y+Z|
 
-  // Only upper triangle needs to be filled
+
+  // 6个点， 2n * 2n, (0-11, 0-11). 最后一个点index（y-index）: 2n-1, 最后一个点x-index: 2n-1-1
+
+  // Only upper triangle needs to be filled， 依赖osqp version >= 0.6.0.
+  // P size 2n * 2n
+  // columns 为csc 所有数据，第一层按列，单列内所有数据（内层为行号+数据）
+  // 第一个vector(外层索引，即col) 代表第几列
+  // 第二个vector（内部，每个元素的pair::c_int 代表行索引）
   std::vector<std::vector<std::pair<c_int, c_float>>> columns;
   columns.resize(num_of_variables_);
+  // 列数统计，所以列数 = 变量数目
   int col_num = 0;
 
+  // 0-1列，(X+Y+Z)
   for (int col = 0; col < 2; ++col) {
     columns[col].emplace_back(col, weight_fem_pos_deviation_ +
                                        weight_path_length_ +
                                        weight_ref_deviation_);
     ++col_num;
   }
-
+  // --  [0,1](-2X-Y)
+  // --  [1,1](5X+2Y+Z)
+  //      2-3 列， 0-1 行
   for (int col = 2; col < 4; ++col) {
     columns[col].emplace_back(
         col - 2, -2.0 * weight_fem_pos_deviation_ - weight_path_length_);
@@ -168,6 +182,7 @@ void FemPosDeviationOsqpInterface::CalculateKernel(
     ++col_num;
   }
 
+  // 第2个点到倒数三个点，列数据是一样的，行index 依次-3
   int second_point_from_last_index = num_of_points_ - 2;
   for (int point_index = 2; point_index < second_point_from_last_index;
        ++point_index) {
@@ -184,9 +199,9 @@ void FemPosDeviationOsqpInterface::CalculateKernel(
       ++col_num;
     }
   }
-
-  int second_point_col_from_last_col = num_of_variables_ - 4;
-  int last_point_col_from_last_col = num_of_variables_ - 2;
+  // 2n-4, 2n-3列
+  int second_point_col_from_last_col = num_of_variables_ - 4;   // 2(n-2), 对应倒数第二个的点x
+  int last_point_col_from_last_col = num_of_variables_ - 2;     // 2(n-1), 对应最后一个点x,
   for (int col = second_point_col_from_last_col;
        col < last_point_col_from_last_col; ++col) {
     columns[col].emplace_back(col - 4, weight_fem_pos_deviation_);
@@ -197,7 +212,7 @@ void FemPosDeviationOsqpInterface::CalculateKernel(
                                        weight_ref_deviation_);
     ++col_num;
   }
-
+  // 2n-2, 2n-1列
   for (int col = last_point_col_from_last_col; col < num_of_variables_; ++col) {
     columns[col].emplace_back(col - 4, weight_fem_pos_deviation_);
     columns[col].emplace_back(
@@ -212,16 +227,17 @@ void FemPosDeviationOsqpInterface::CalculateKernel(
 
   int ind_p = 0;
   for (int i = 0; i < col_num; ++i) {
-    P_indptr->push_back(ind_p);
+    P_indptr->push_back(ind_p);  // 累加每列非零元素计数， 从0开始， 与matrix 索引无关
     for (const auto& row_data_pair : columns[i]) {
       // Rescale by 2.0 as the quadratic term in osqp default qp problem setup
       // is set as (1/2) * x' * P * x
-      P_data->push_back(row_data_pair.second * 2.0);
-      P_indices->push_back(row_data_pair.first);
+      P_data->push_back(row_data_pair.second * 2.0);  // 按列排序，非零数据， 上三角
+      P_indices->push_back(row_data_pair.first);  // 行index
       ++ind_p;
     }
+    // i列有几个元素，p_indptr[i] - p_indptr[i-1]
   }
-  P_indptr->push_back(ind_p);
+  P_indptr->push_back(ind_p); // 最后一个index 要大1
 }
 
 void FemPosDeviationOsqpInterface::CalculateOffset(std::vector<c_float>* q) {
@@ -236,10 +252,14 @@ void FemPosDeviationOsqpInterface::CalculateAffineConstraint(
     std::vector<c_float>* A_data, std::vector<c_int>* A_indices,
     std::vector<c_int>* A_indptr, std::vector<c_float>* lower_bounds,
     std::vector<c_float>* upper_bounds) {
+  // A 是单位矩阵
   int ind_A = 0;
   for (int i = 0; i < num_of_variables_; ++i) {
+    // element
     A_data->push_back(1.0);
+    // element-row-index
     A_indices->push_back(i);
+    // 累计
     A_indptr->push_back(ind_A);
     ++ind_A;
   }
@@ -272,12 +292,14 @@ bool FemPosDeviationOsqpInterface::OptimizeWithOsqp(
     std::vector<c_float>* q, std::vector<c_float>* primal_warm_start,
     OSQPData* data, OSQPWorkspace** work, OSQPSettings* settings) {
   CHECK_EQ(lower_bounds->size(), upper_bounds->size());
-
+  // n: 变量个数, m: 约束个数
   data->n = kernel_dim;
+  // m: 约束个数，点数*2, 因为每个点有x,y,  这里n=m
   data->m = num_affine_constraint;
   data->P = csc_matrix(data->n, data->n, P_data->size(), P_data->data(),
                        P_indices->data(), P_indptr->data());
   data->q = q->data();
+  // csc 输入， m 行数， n 列数
   data->A = csc_matrix(data->m, data->n, A_data->size(), A_data->data(),
                        A_indices->data(), A_indptr->data());
   data->l = lower_bounds->data();
